81d152e7d0a363af68cb3648cf519a4fdc7d819d
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-03-17 15:43:08 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function (req, res, next) {
+    var _cookie = res.cookie;
+    res.cookie = function cookie(name, value, options) {
+        options = options || {};
+        if (typeof options.secure === 'undefined') {
+            options.secure = (req.protocol === 'https');
+        }
+        _cookie(name, value, options);
+    };
+};
